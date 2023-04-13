8d0936fd52d2b2b66722d3ddad3a00663f080a9d
Maria Lipina <m.o.lip714@gmail.com>
2023-04-13 11:38:12 +0300

    Добавила обучение, методы поиска, исправила switch

--- src/main/java/farm/Main.java
+++ src/main/java/farm/Main.java
@@ -4,7 +4,6 @@ import farm.utils.Controller;
 
 public class Main {
     public static void main(String[] args) {
-        new Controller().run();
-
+//        new Controller().run();
     }
 }
--- src/main/java/farm/utils/Farm.java
+++ src/main/java/farm/utils/Farm.java
@@ -1,6 +1,7 @@
 package farm.utils;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 class Farm {
     ArrayList<Animal> animals;
@@ -16,21 +17,21 @@ class Farm {
     void addAnimal(String [] animal) {
 
         switch (animal[0]) {
-            case "Dog":
-                animals.add(new Dog(animal));  break;
-            case "Cat":
-                animals.add(new Cat(animal));  break;
-            case "Hamster":
-                animals.add(new Hamster(animal));  break;
-            case "Camel":
+            case "Dog" ->
+                animals.add(new Dog(animal));
+            case "Cat" ->
+                animals.add(new Cat(animal));
+            case "Hamster" ->
+                animals.add(new Hamster(animal));
+            case "Camel" ->
                 animals.add(new Camel(animal,
-                    Integer.parseInt(searchOption(packsOptions, 3, "верблюд"))));  break;
-            case "Horse":
+                    Integer.parseInt(searchOption(packsOptions, 2, "верблюд"))));
+            case "Horse" ->
                 animals.add(new Horse(animal,
-                    Integer.parseInt(searchOption(packsOptions, 3, "лошадь"))));  break;
-            case "Donkey":
+                    Integer.parseInt(searchOption(packsOptions, 2, "лошадь"))));
+            case "Donkey" ->
                 animals.add(new Donkey(animal,
-                    Integer.parseInt(searchOption(packsOptions, 3, "осел"))));  break;
+                    Integer.parseInt(searchOption(packsOptions, 2, "осел"))));
         }
 
         if (animals.size() > 0) System.out.println("Добавленное животное: " + animals.get(animals.size() -1));
@@ -43,20 +44,52 @@ class Farm {
         return "-1";
     }
 
+    LinkedList<Animal> searchByName(String animalName) {
+        LinkedList<Animal> result = new LinkedList<>();
+        for (Animal anim: animals) {
+            if (anim.getName().equals(animalName)) {
+                result.add(anim);
+            }
+        }
+        return result;
+    }
+
+    LinkedList<Animal> searchByCategory (String category) {
+        LinkedList<Animal> result = new LinkedList<>();
+        for (Animal anim: animals) {
+            if (
+                (category.equals("pet") && anim instanceof PetAnimal) ||
+                (category.equals("pack") && anim instanceof PackAnimal)
+            )   result.add(anim);
+        }
+        return result;
+    }
+
     Animal canLearnCommand(String animalName, String command) {
         for (Animal anim: animals) {
             if (anim.getName().equals(animalName)) {
                 if(anim.getCommands().contains(command)) return null;
                 if(anim instanceof PetAnimal) {
-                    if (searchOption(petsOptions, 2, anim.getClass().getSimpleName())
+                    if (searchOption(petsOptions, 1, anim.getClass().getSimpleName())
                             .contains(command)) return anim;
                 }
                 if(anim instanceof PackAnimal) {
-                    if (searchOption(packsOptions, 2, anim.getClass().getSimpleName())
+                    if (searchOption(packsOptions, 1, anim.getClass().getSimpleName())
                             .contains(command)) return anim;
                 }
             }
         }
         return null;
+    }
+
+    String teach (String animalName, String command) {
+        Animal student = canLearnCommand(animalName, command);
+        if (student != null) {
+            student.learnCommand(command);
+            return String.format("Success! Теперь %s знает такие команды: %s", animalName, student.getCommands());
+        }
+        else {
+            return String.format("%s не может выполнять команду %s", animalName, command);
+        }
     }
 }
